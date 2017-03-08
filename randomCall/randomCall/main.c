dfc528717b894eb2b713d187fbb8ac1a51e9eaa6
wjcgithub <wjc163@sina.cn>
2017-03-08 09:50:31 +0800

    first add

---
+++ randomCall/randomCall/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100  //最大学生数
+#define CMAX 4   //需要点名的次数
+
+int main()
+{
+    char stuName[MAX][20];
+    char stuID[MAX][10];
+    int stuScore[MAX];  //出勤分数
+    int stuAbsentNum[MAX] = {0}; // 学生的缺勤次数
+    int stuNum, i;
+
+
+
+    return 0;
+}
