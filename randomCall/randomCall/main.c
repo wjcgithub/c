5f190300225a8a9d4d0552f1df68558872ecc0e1
wjcgithub <wjc163@sina.cn>
2017-03-14 18:10:16 +0800

    整理

--- randomCall/randomCall/main.c
+++ randomCall/randomCall/main.c
@@ -3,6 +3,26 @@
 #define MAX 100  //最大学生数
 #define CMAX 4   //需要点名的次数
 
+void RandomCall(char stuName[][20], char stuID[][10], int stuAbsentNum[]);
+void SumScore(char stuName[][20], char stuID[][10], int stuAbsentNum[]);
+void OutputInfo(char stuName[][20], char stuID[][10], int stuScore[]);
+
+int InputInfo(char stuName[][20], char stuID[][10])
+{
+    int stuNum, i;
+    printf("Please input the number of students. \n");
+    scanf("%d", &stuNum);
+    getchar();
+    printf("Please input the information of students.\n");
+    for (i=0; i<stuNum; i++)
+    {
+        scanf("%s",stuID[i]);
+        scanf("%s",stuName[i]);
+    }
+
+    return stuNum;
+}
+
 int main()
 {
     char stuName[MAX][20];
@@ -20,6 +40,7 @@ int main()
     }
     //统计缺勤次数
     SumScore(stuName, stuID, stuAbsentNum);
+    //输出缺勤信息
     OutputInfo(stuName, stuID, stuScore);
 
     return 0;
